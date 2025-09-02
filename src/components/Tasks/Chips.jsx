97b93a7acce9f5ecb1ab769c63f4dfd8dc5c345d
TAURUS-ESSEN <reitarovea@gmail.com>
2025-09-02 23:37:04 +0200

    refactor(chips): simplify chips.jsx

--- src/components/Tasks/Chips.jsx
+++ src/components/Tasks/Chips.jsx
@@ -4,15 +4,18 @@ import styles from './tasks.module.css';
 
 export default function Chips() {
 
-    const {categories, filters, setFilters} = useOutletContext ();
+    const {categories, filters, setFilters} = useOutletContext();
     const [chips, setChips] = useState([])
-        const DEFAULT = {
+    const category = categories.find(c => c.id === Number(filters.category))
+
+    const DEFAULT = {
         status: `Status: ${filters.status}`,
-        category: 'filter by category',
-        period: `${filters.period} tasks`,
-        search: `filter by ${filters.search}`,
+        category: `Category: ${category?.name ?? 'No category' }`,
+        period: `Time: ${filters.period}`,
+        search: `Search word: ${filters.search}`,
         priority: `Priority: ${filters.priority}`
     }
+
     function resetCurrentFilter(value) {
         value === 'search'
         ? setFilters(prev=>({...prev, [value]: ''}))
@@ -22,32 +25,30 @@ export default function Chips() {
     
     useEffect(() => {
         for (let key in filters) {
-            if (filters[key] !=='all' && filters[key] !=='') {
+            if (filters[key] !== 'all' && filters[key] !== '') {
                 if (!chips.includes(key)) {
-                    console.log(key)
-                    console.log(chips)
                     setChips(prev => ([...prev, key]))
                 }
             }
-            if (filters[key] ==='all' || filters[key] ==='') {
-                if (chips.includes(key)) { 
-                    setChips(prev=>prev.filter(c=>c!==key))
-                }
-            }
+            // if (filters[key] ==='all' || filters[key] === '') {
+            //     if (chips.includes(key)) { 
+            //         setChips(prev => prev.filter(c => c !== key))
+            //     }
+            // }
+            else setChips(prev => prev.filter(c => c !== key))
         }
     },[filters])
 
     return (
     <>  
-        <div className={styles.chipsBlock}>
-        {   
-            chips.map(c=>{
+    <div className={styles.chipsBlock}>
+        {chips.map(c => {
                 return (
                     <>
                         <span className={styles.chipName}>
                             {DEFAULT[c] ?? String(c)}
                             <button 
-                                onClick={(e)=>resetCurrentFilter(e.target.value)} 
+                                onClick={(e) => resetCurrentFilter(e.target.value)} 
                                 value={c} 
                                 className={styles.deleteChipBtn}
                             >
@@ -56,8 +57,7 @@ export default function Chips() {
                         </span>
                     </>
                 )
-            }) 
-        }
+        })}
     </div>
     </>
 )
